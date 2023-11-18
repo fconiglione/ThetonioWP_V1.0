4bbc4766e8a69baa681f6d9b1bdb43640c0400c6
fconiglione <122475578+fconiglione@users.noreply.github.com>
2023-11-18 14:31:05 -0500

    fixed create-project.php and added a new image for a new project

--- create-project.php
+++ create-project.php
@@ -1,56 +1,56 @@
 <?php
-    require('inc/auth.php');
+require('inc/auth.php');
 
-    $preview = $_POST['preview'];;
-    $previewAlt = $_POST['previewAlt'];
-    $projectTitle = $_POST['projectTitle'];
-    $projectDescription = $_POST['projectDescription'];
-    $projectLink = $_POST['projectLink'];
-    $gitHubLink = $_POST['gitHubLink'];
-    $ok = true;
+$preview = $_POST['preview'];
+$previewAlt = $_POST['previewAlt'];
+$projectTitle = $_POST['projectTitle'];
+$projectDescription = $_POST['projectDescription'];
+$gitHubLink = $_POST['gitHubLink'];
+$projectLink = $_POST['projectLink'];
+$ok = true;
 
-    if (empty($preview)) {
-        echo '<p>File path is required.</p>';
-        $ok = false;
-    }
-    if (empty($previewAlt)) {
-        echo '<p>Alt text is required.</p>';
-        $ok = false;
-    }
-    if (empty($projectTitle)) {
-        echo '<p>Project title is required.</p>';
-        $ok = false;
-    }
-    if (empty($projectDescription)) {
-        echo '<p>Project description is required.</p>';
-        $ok = false;
-    }
-    if (empty($gitHubLink)) {
-        echo '<p>GitHub link is required.</p>';
-        $ok = false;
-    }
-    if (empty($projectLink)) {
-        echo '<p>Project link is required.</p>';
-        $ok = false;
-    }
+if (empty($preview)) {
+    echo '<p>Alt text is required.</p>';
+    $ok = false;
+}
+if (empty($previewAlt)) {
+    echo '<p>Alt text is required.</p>';
+    $ok = false;
+}
+if (empty($projectTitle)) {
+    echo '<p>Project title is required.</p>';
+    $ok = false;
+}
+if (empty($projectDescription)) {
+    echo '<p>Project description is required.</p>';
+    $ok = false;
+}
+if (empty($gitHubLink)) {
+    echo '<p>GitHub link is required.</p>';
+    $ok = false;
+}
+if (empty($projectLink)) {
+    echo '<p>Project link is required.</p>';
+    $ok = false;
+}
 
-    if ($ok) {
-        require('inc/db.php');
-        $sql = "INSERT INTO projects (preview, previewAlt, projectTitle, projectDescription, projectLink) 
-        VALUES (:preview, :previewAlt, :projectTitle, :projectDescription, :projectLink)";
+if ($ok) {
+    require('inc/db.php');
+    $sql = "INSERT INTO projects (preview, previewAlt, projectTitle, projectDescription, gitHubLink, projectLink) 
+    VALUES (:preview, :previewAlt, :projectTitle, :projectDescription, :gitHubLink, :projectLink)";
 
-        $cmd = $db->prepare($sql);
-        $cmd->bindParam(':preview', $preview, PDO::PARAM_STR, 255);
-        $cmd->bindParam(':previewAlt', $previewAlt, PDO::PARAM_STR, 255);
-        $cmd->bindParam(':projectTitle', $projectTitle, PDO::PARAM_STR, 255);
-        $cmd->bindParam(':projectDescription', $projectDescription, PDO::PARAM_STR);
-        $cmd->bindParam(':gitHubLink', $gitHubLink, PDO::PARAM_STR, 255);
-        $cmd->bindParam(':projectLink', $projectLink, PDO::PARAM_STR, 255);
+    $cmd = $db->prepare($sql);
+    $cmd->bindParam(':preview', $preview, PDO::PARAM_STR, 255);
+    $cmd->bindParam(':previewAlt', $previewAlt, PDO::PARAM_STR, 255);
+    $cmd->bindParam(':projectTitle', $projectTitle, PDO::PARAM_STR, 255);
+    $cmd->bindParam(':projectDescription', $projectDescription, PDO::PARAM_STR);
+    $cmd->bindParam(':gitHubLink', $gitHubLink, PDO::PARAM_STR, 255);
+    $cmd->bindParam(':projectLink', $projectLink, PDO::PARAM_STR, 255);
 
-        $cmd->execute();
-        $db = null;
+    $cmd->execute();
+    $db = null;
 
-        header('Location: https://thetonio.com/admin-portfolio/');
-        exit();
-    }
+    header('Location: https://thetonio.com/admin-portfolio/');
+    exit();
+}
 ?>
